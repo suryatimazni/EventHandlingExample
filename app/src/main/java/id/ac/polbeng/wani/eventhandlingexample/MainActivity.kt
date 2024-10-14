8f6583e59b152ff6f43384ad347dbebe31057145
Monaratuliu05 <monaratuliumona05@gmail.com>
2024-10-14 16:05:51 +0700

    add commit

---
+++ app/src/main/java/id/ac/polbeng/wani/eventhandlingexample/MainActivity.kt
@@ -0,0 +1,28 @@
+package id.ac.polbeng.wani.eventhandlingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import id.ac.polbeng.wani.eventhandlingexample.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnDisplayMessage.setOnClickListener {
+            Toast.makeText(
+                applicationContext,
+                "Hello World",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        binding.btnDisplayMessage.setOnLongClickListener {
+
+            Snackbar.make(binding.rootLayout, "Long Click",
+                Snackbar.LENGTH_LONG).show()
+            true
+        }
+    }
+}
